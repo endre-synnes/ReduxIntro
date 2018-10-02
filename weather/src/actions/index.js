4ebc1792d8bbcffece84e331a154ef972f638a99
Endre Synnes <synend16@student.westerdals.no>
2018-10-02 21:22:16 +0200

    added enviroment variables and updated ignore file  etc

--- weather/src/actions/index.js
+++ weather/src/actions/index.js
@@ -1,12 +1,13 @@
 import axios from "axios";
+import api from "../env/api-key"
 
-const API_KEY = "6a78596d062df78380eff5944c4e5567";
+const API_KEY = api;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = "FETCH_WEATHER";
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
+  const url = `${ROOT_URL}&q=${city},no`;
   const request = axios.get(url);
 
   return {
