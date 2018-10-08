2af300934f19819846e91e95f8f1d689f6d2af56
Endre Synnes <synend16@student.westerdals.no>
2018-10-08 23:39:20 +0200

    more on blog

--- blog/src/components/posts_index.js
+++ blog/src/components/posts_index.js
@@ -1,7 +1,7 @@
 import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
@@ -13,15 +13,35 @@ class PostsIndex extends Component {
   render() {
     return (
       <div>
-        Posts Index
+        <div className="text-xs-right">
+          <Link className="btn btn-primary" to="/posts/new">
+            Add a Post
+          </Link>
+        </div>
+        <h3>Posts</h3>
+        <ul className="list-group">
+          {this.renderPosts()}
+        </ul>
       </div>
     )
   }
 
+  renderPosts() {
+    return _.map(this.props.posts, post => {
+      return (
+        <li className="list-group-item" key={post.id}>
+          {post.title}
+        </li>
+      )
+    });
+  }
 }
 
+function mapStateToProps(state) {
+  return { posts: state.posts };
+}
 
-
+export default connect(mapStateToProps, { fetchPosts })(PostsIndex);
 
 
 
@@ -55,8 +75,5 @@ class PostsIndex extends Component {
   // }
 
 
-// function mapStateToProps(state) {
-//   return { posts: state.posts };
-// }
 
-export default connect(null, { fetchPosts })(PostsIndex);
+
